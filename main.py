5efa9c35df9683867a6271c5d8faed9d8fcf1f86
idan fr <idan441@gmail.com>
2022-01-25 19:28:35 +0200

    Added basic options for buy or sell products

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from typing import List
-from classes.products import Product
+from classes.products import Product, PlayersInventory, PlayerProductInventory
 from constants import CITIES_LIST, PRODUCTS_LIST, INITIAL_BUDGET, AMOUNT_OF_HOURS_FOR_WORKDAY
-
-
 
 
 class GameResults:
@@ -100,6 +98,7 @@ class Game:
 		self.player = Player(name="example",
 							 initial_budget=INITIAL_BUDGET,
 							 initial_location="Yafo")
+		self.player_inventory = PlayersInventory(products_list_in_game=PRODUCTS_LIST)
 		self.ship = Ship()
 		self.current_trade_day: int = 0
 		self.hours_left_for_workday: int = 16
@@ -114,21 +113,95 @@ class Game:
 		""" Will start a game and manage it until the end """
 		self.start_game_message()
 
-		while self.current_trade_day <= self.last_trade_day:
+		while self.is_last_trade_day is not True:
+			if self.current_trade_day == self.last_trade_day:
+				print("This is the last day of trade! Make sure to sell any products left in your ship!")
+				self.is_last_trade_day = True
+
 			self.print_current_game_status()
+			self.manage_trade_day_menu()
 			self.move_to_next_day()
 
 		self.end_game()
 		return None
 
-	def move_to_next_day(self):
-		""" Moves to next day of trade """
+	def manage_trade_day_menu(self):
+		""" Allows player to manage options of a trade day.
+
+		:return:
+		"""
+		print("It's morning of a new trade day. ")
+		while True:
+			print("Choose an action:"
+				  "1 - Trade products"
+				  "2 - show inventory"
+				  "3 - Finish trade day")
+			option_chose: int = int(input())
+			if option_chose == 1:
+				self.trade_products_menu()
+			elif option_chose == 2:
+				self.print_inventory()
+			elif option_chose == 3:
+				break
+			else:
+				print("Wrong option was chosen - try again")
+
+		print("The trade day has finished, you go to sleep.")
+		return None
+
+	def trade_products_menu(self) -> None:
+		""" Manages menu for trading (buying/selling) products.
+
+		:return: None
+		"""
+		print("Choose a product name to trade")
+		product_name: str = input()
+
+		product_details: PlayerProductInventory = self.player_inventory.get_product_by_name(product_name=product_name)
+		print(f"You currently have {product_details.amount} of {product_details.product_name}")
+
+		print("Do you want to buy or sell? (Options - buy or sell)")
+		action: str = input()
+
+		print(f"Choose amount you want to {action}")
+		amount_to_buy_or_sell: int = int(input())
+
+		if action == "buy":
+			print(f"Buying {amount_to_buy_or_sell} {product_details.product_name}")
+			self.player_inventory.add_product_to_inventory(product=product_details.product,
+														   amount_to_add=amount_to_buy_or_sell)
+		elif action == "sell":
+			print(f"Buying {amount_to_buy_or_sell} {product_details.product_name}")
+			self.player_inventory.remove_product_from_inventory(product=product_details.product,
+																amount_to_remove=amount_to_buy_or_sell)
+		else:
+			print("Wrong option chosen! Try again...")
+
+		print("You just bought/sold!")
+
+		return None
+
+	def print_inventory(self) -> None:
+		""" Prints the player's inventory
+
+		:return None:
+		"""
+		products_inventory: List[PlayerProductInventory] = self.player_inventory.get_inventory_content()
+
+		print("Current inventory: ")
+		for product in products_inventory:
+			print(f"{product.product_name}: {product.amount}")
+
+		return None
+
+	def move_to_next_day(self) -> None:
+		""" Moves to next day of trade
+
+		 :return:
+		 """
 		self.hours_left_for_workday = AMOUNT_OF_HOURS_FOR_WORKDAY
 		self.current_trade_day += 1
-
-		if self.current_trade_day == self.is_last_trade_day:
-			print("This is the last day of trade! Make sure to sell any products left in your ship!")
-			self.is_last_trade_day = True
+		return None
 
 	def start_game_message(self):
 		""" Prints details for the first time the game starts """
@@ -163,6 +236,7 @@ def main():
 	print("Sea Trader is a homage to Socher HaYam")
 	game = Game()
 	game.start_game()
+
 
 if __name__ == "__main__":
 	main()
